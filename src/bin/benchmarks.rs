2a33fa390da5bf3754ee896cbbde313d8b929187
Alex Cani <alexcani109@gmail.com>
2024-01-11 19:37:45 -0300

    Add per test case output in benchmarks

--- src/bin/benchmarks.rs
+++ src/bin/benchmarks.rs
@@ -13,7 +13,8 @@ fn read_lines(filename: &str) -> Vec<String> {
 // Run a benchmark with input from a file. Each line in a file contains the sequence of moves
 // and the expected score the engine should evaluate to
 // Outputs the average time taken to solve position, avg number of nodes searched, and avg node search rate.
-fn benchmark(file: &str) {
+fn benchmark(file: &str, per_case_output: bool) {
+    println!("Running benchmark: {}", file);
     let mut times = Vec::new();
     let mut nodes_searched = Vec::new();
     let lines = read_lines(file);
@@ -33,18 +34,46 @@ fn benchmark(file: &str) {
         if result.score == expected_score {
             n_correct += 1;
         }
+
+        if per_case_output {
+            println!(
+                "Game #{}: {} - {}us - {} nodes - {} Kpos/s",
+                times.len(),
+                if result.score == expected_score {
+                    "PASSED"
+                } else {
+                    "FAILED"
+                },
+                elapsed,
+                result.nodes_searched,
+                result.nodes_searched as f32 / elapsed as f32 * 1_000.0
+            );
+        }
     }
 
     println!("Benchmark result: {}", file);
     println!("Number of entries: {}", times.len());
-    println!("Number of correct scores: {} ({:.2}%)", n_correct, n_correct as f32 / times.len() as f32 * 100.0);
-    println!("Average time taken: {}μs", times.iter().sum::<u128>() as f32 / times.len() as f32);
-    println!("Average nodes searched: {}", nodes_searched.iter().sum::<usize>() as f32 / nodes_searched.len() as f32);
-    println!("Average nodes searched per second: {} Kpos/s", nodes_searched.iter().sum::<usize>() as f32 / times.iter().sum::<u128>() as f32 * 1_000.0);
+    println!(
+        "Number of correct scores: {} ({:.2}%)",
+        n_correct,
+        n_correct as f32 / times.len() as f32 * 100.0
+    );
+    println!(
+        "Average time taken: {}μs",
+        times.iter().sum::<u128>() as f32 / times.len() as f32
+    );
+    println!(
+        "Average nodes searched: {}",
+        nodes_searched.iter().sum::<usize>() as f32 / nodes_searched.len() as f32
+    );
+    println!(
+        "Average nodes searched per second: {} Kpos/s",
+        nodes_searched.iter().sum::<usize>() as f32 / times.iter().sum::<u128>() as f32 * 1_000.0
+    );
 }
 
 fn main() {
-    benchmark("benchmarks/Test_L3_R1.txt");  // End game - Easy
+    benchmark("benchmarks/Test_L3_R1.txt", false); // End game - Easy
     println!("==============================");
-    //benchmark("benchmarks/Test_L2_R1.txt");  // Mid game - Easy
+    //benchmark("benchmarks/Test_L2_R1.txt", true);  // Mid game - Easy
 }
