9ad57ef081eb41ce4cc52151063736208ea17b77
Alex Cani <alexcani109@gmail.com>
2024-01-22 15:18:30 -0300

    Create TranspositionTable

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod board;
 pub mod solver;
+mod transposition_table;
 
 pub mod prelude {
     pub use crate::board::*;
     pub use crate::solver::*;
+    pub use crate::transposition_table::TranspositionTable;
 }
---
+++ src/transposition_table.rs
@@ -0,0 +1,50 @@
+use static_assertions as sa;
+use modular_bitfield::prelude::*;
+
+#[bitfield]
+#[derive(Clone, Copy)]
+struct Entry {
+    key: B56,
+    score: u8,
+}
+
+sa::assert_eq_size!(Entry, u64);
+
+/// A transposition table is a cache of previously computed positions.
+/// It is used to avoid recomputing the same position multiple times.
+/// The table is indexed by a hash of the position, and stores the score of the position for the current player.
+/// To create a new table with the default size of 64MB, use [`TranspositionTable::default`], otherwise use [`TranspositionTable::new`] with
+/// the desired number of entries (each entry is 8 bytes long). Ideally the table size should be a prime number.
+pub struct TranspositionTable {
+    table: Vec<Entry>,
+}
+
+impl TranspositionTable {
+    pub fn new(size: usize) -> Self {
+        Self {
+            table: vec![Entry::from_bytes([0u8; 8]); size],
+        }
+    }
+
+    pub fn get(&self, key: u64) -> Option<u8> {
+        let index = key as usize % self.table.len();
+        let entry = self.table[index];
+        if entry.key() == key {
+            Some(entry.score())
+        } else {
+            None
+        }
+    }
+
+    pub fn set(&mut self, key: u64, score: u8) {
+        let index = key as usize % self.table.len();
+        self.table[index] = Entry::new().with_key(key).with_score(score);
+    }
+}
+
+impl Default for TranspositionTable {
+    fn default() -> Self {
+        // 8388617 is the closest prime number that will result in a 64MB table
+        Self::new(8388617)
+    }
+}
