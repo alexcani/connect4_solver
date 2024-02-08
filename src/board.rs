1d5fc820340c59b45728b2b089588f4c0678a964
Alex Cani <alexcani109@gmail.com>
2024-02-08 02:37:47 +0100

    Fix tests after previous change

--- src/board.rs
+++ src/board.rs
@@ -392,19 +392,19 @@ mod tests {
     fn test_possible_nonlosing_moves() {
         let board = BitBoard::new();
         let moves = board.possible_nonlosing_moves();
-        assert_eq!(moves, [true; WIDTH]); // all columns are possible
+        assert_eq!(moves, Some([true; WIDTH])); // all columns are possible
 
         // Losing position
         let board = BitBoard::from_notation("4453623221115");
 
         // Player 2's turn, but player 1 can win in A or G. So there's nothing player 2 can do
-        assert_eq!(board.possible_nonlosing_moves(), [false; WIDTH]); // all columns are false
+        assert_eq!(board.possible_nonlosing_moves(), None); // all columns are false
 
         // Player 1 can win in E
         let mut board = BitBoard::from_notation("2334465545");
         assert!(board.is_winning(Column::E));
         board.play(Column::A); // don't win yet
-        assert_eq!(board.possible_nonlosing_moves(), [false, false, false, false, true, false, false]); // only E is possible otherwise p1 wins
+        assert_eq!(board.possible_nonlosing_moves(), Some([false, false, false, false, true, false, false])); // only E is possible otherwise p1 wins
     }
 
     #[test]
